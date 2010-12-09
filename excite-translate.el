51ef3bd686d5a910f8c24391db875efd168944f4
lieutar <lieutar@1dk.jp>
2010-12-10 00:42:28 +0900

    結果バッファをちょっとリッチにした

--- excite-translate.el
+++ excite-translate.el
@@ -46,6 +46,8 @@
 (defun excite-translate-enja (before) (excite-translate-* before "ENJA"))
 (defun excite-translate-jaen (before) (excite-translate-* before "JAEN"))
 
+(defvar excite-translate-previous-window-configuration nil)
+(defvar excite-translate-end-of-before nil)
 (defvar excite-translate-guess-type-threshold 0.25)
 (defun excite-translate-guess-type (str)
   (let ((ascii     0)
@@ -67,38 +69,86 @@
 
 (defconst excite-translate-result-mode-map
   (let ((km (make-sparse-keymap)))
-    (define-key km (kbd "q") 'excite-translate-result-mode-quit)
+    (define-key km (kbd "C-c C-k") 'excite-translate-result-mode-quit)
+    (define-key km (kbd "C-c C-c") 'excite-translate-result-mode-re-translate)
     km))
 
 (define-derived-mode excite-translate-result-mode
   text-mode
   "excite-translate-result"
+  "The Major mode for displaying result of \\[excite-translate].
+
+    \\[excite-translate-result-mode-map]
+"
   (make-variable-buffer-local
-   'excite-translate-result-mode-back-to))
+   'excite-translate-result-mode-back-to)
+  (make-variable-buffer-local
+   'excite-translate-previous-window-configuration)
+  (make-variable-buffer-local
+   'excite-translate-end-of-before))
 
 (defun excite-translate-result-mode-quit ()
   (interactive)
-  (let ((backto (and (windowp excite-translate-result-mode-back-to)
+  (let ((winconf excite-translate-previous-window-configuration)
+        (backto (and (windowp excite-translate-result-mode-back-to)
                      excite-translate-result-mode-back-to)))
     (kill-buffer (current-buffer))
-    (when backto
-      (select-window backto))))
+    (when winconf (set-window-configuration winconf))
+    (when backto (select-window backto))))
+
+(defun excite-translate-result-mode-re-translate ()
+  (interactive)
+  (excite-translate
+   (replace-regexp-in-string
+    "\\`\\s +\\|\\s +\\'" ""
+    (buffer-substring-no-properties
+     (point-min) excite-translate-end-of-before))
+    nil
+    t))
 
 (defun excite-translate-popup (before result)
   (let ((buf (get-buffer-create "*excite-translate*")))
     (set-buffer buf)
-    (setq buffer-read-only t)
-    (let ((buffer-read-only nil))
-      (delete-region (point-min) (point-max))
-      (insert before)
-      (insert "\n----\n")
-      (insert result)
-      (goto-char (point-min)))
     (excite-translate-result-mode)
+    (let ((inhibit-read-only t)
+          buffer-read-only)
+      (delete-region (point-min) (point-max))
+      (insert "\n" before "\n")
+      (setq excite-translate-end-of-before (point-marker))
+      (setq excite-translate-previous-window-configuration
+            (current-window-configuration (selected-frame)))
+      (insert (propertize "--------\n"
+                          'face '((:foreground "#CCCCCC"
+                                   :background "#CCCCCC"))
+                          'read-only t
+                          'rear-nonsticky t))
+      (insert (propertize (concat "\n"
+                                  (propertize result
+                                              'face
+                                              '((:foreground "#000099"))) "\n")
+                          'read-only t))
+      (goto-char (point-min)))
     (setq excite-translate-result-mode-back-to cwin)
     (pop-to-buffer buf)))
 
-(defun excite-translate (before &optional type)
+(defun excite-translate (before &optional type with-popup)
+  "Translates string by Excite translation service.
+
+`BEFORE' is the string for translation. `TYPE' is translation type.
+When the `TYPE' is \"ENJA\" , the translation is English to Japanese, 
+when the `TYPE' is \"JAEN\" , the translation is Japanese to English.
+If the `TYPE' is nil, this function chooses value of `TYPE' automatically.
+
+When `WITH-POPUP' is not nil this function popups buffer that contains
+result of the translation. The mode of the buffer is
+`excite-translate-result-mode'.
+
+
+Under the interactive calling:
+
+  If the region is active, this command uses string at content of the region.
+  Otherwise this command asks string with prompt at the minibuffer.
+"
   (interactive 
    (list (if mark-active
              (buffer-substring-no-properties
@@ -107,10 +157,10 @@
 
   (let* ((cwin   (selected-window))
          (type   (or type
-                     (excite-translate-guess-type before)
-                     ))
+                     (excite-translate-guess-type before)))
          (result (excite-translate-* before type)))
-    (when (interactive-p)
+    (when (or with-popup
+              (interactive-p))
       (excite-translate-popup before result))
     result))
 
